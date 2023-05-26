1f75352bfb7ee60ad19d3abbc31df70c8b5f03d5
Mohammad Zafar <mzcoder7@gmail.com>
2023-05-26 15:52:06 +0100

    added weather icons

--- weather-app/src/Components/InputCity.tsx
+++ weather-app/src/Components/InputCity.tsx
@@ -1,65 +1,74 @@
-import React, { useState } from "react";
-import "./App";
+import React, { useState } from 'react';
+import './App';
 // import moment from 'moment';
 // import GetWeather from './WeatherDisplay';
-import WeatherDisplay from "./WeatherDisplay";
+import WeatherDisplay from './WeatherDisplay';
 const InputCity = () => {
-  // Initial goal to render:
-  // Name of city, temp, humidity, sunrise and sunset, description (clouds)
-  const [city, setCity] = useState<string>("");
-  const [name, setName] = useState<string>("");
-  const [description, setDescription] = useState<string>("");
-  const [temp, setTemp] = useState<number>(0);
-  const [humidity, setHumidity] = useState<number>(0);
-  const [success, setSuccess] = useState<boolean>(false);
-  const [clicked, setClicked] = useState<boolean>(false);
-  // const [sunrise, setSunrise] = useState<string>();
-  // const [sunset, setSunset] = useState<string>();
-  // function to fetch weather info
-  async function getWeather() {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9426d32b582da23208df385085a515cd`
-    );
-    const data = await response.json();
-    console.log(data);
-    if (response.status === 200) {
-      setName(data.name);
-      console.log(name);
-      setDescription(data.weather[0].description);
-      setTemp(Math.round(data.main.temp - 273.15));
-      console.log(temp);
-      setHumidity(data.main.humidity);
-      console.log(humidity);
-      setSuccess(true);
-    } else {
-      setSuccess(false);
-    }
-    setClicked(true);
-  }
-  return (
-    <>
-      <input
-	  id='input'
-        type="text"
-        placeholder="Enter city"
-        onKeyUp={(e: React.KeyboardEvent<HTMLInputElement>) => {
-          setCity(e.currentTarget.value);
-          console.log(city);
-        }}
-      />
-      <button id='button' onClick={getWeather}>Submit</button>
-      {success === true && clicked === true ? (
-        <WeatherDisplay
-          name={name}
-          description={description}
-          temp={temp}
-          humidity={humidity}
-        />
-      ) : success === false && clicked === true ? (
-        <p>City not found</p>
-      ) : ""
+	// Initial goal to render:
+	// Name of city, temp, humidity, sunrise and sunset, description (clouds)
+	const [city, setCity] = useState<string>('');
+	const [name, setName] = useState<string>('');
+	const [description, setDescription] = useState<string>('');
+	const [temp, setTemp] = useState<number>(0);
+	const [humidity, setHumidity] = useState<number>(0);
+	const [success, setSuccess] = useState<boolean>(false);
+	const [clicked, setClicked] = useState<boolean>(false);
+	const [icon, setIcon] = useState<string>('');
+	// const [sunrise, setSunrise] = useState<string>();
+	// const [sunset, setSunset] = useState<string>();
+	// function to fetch weather info
+	async function getWeather() {
+		const response = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9426d32b582da23208df385085a515cd`
+		);
+		const data = await response.json();
+		console.log(data);
+		if (response.status === 200) {
+			// data.weather[0].icon
+			setIcon(
+				`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+			);
+			setName(data.name);
+			console.log(name);
+			setDescription(data.weather[0].description);
+			setTemp(Math.round(data.main.temp - 273.15));
+			console.log(temp);
+			setHumidity(data.main.humidity);
+			console.log(humidity);
+			setSuccess(true);
+		} else {
+			setSuccess(false);
+		}
+		setClicked(true);
 	}
-    </>
-  );
+	return (
+		<>
+			<input
+				id='input'
+				type='text'
+				placeholder='Enter city'
+				onKeyUp={(e: React.KeyboardEvent<HTMLInputElement>) => {
+					setCity(e.currentTarget.value);
+					console.log(city);
+				}}
+			/>
+			<button id='button' onClick={getWeather}>
+				Submit
+			</button>
+			{success === true && clicked === true ? (
+				<WeatherDisplay
+					icon={icon}
+					name={name}
+					description={description}
+					temp={temp}
+					humidity={humidity}
+				/>
+			) : success === false && clicked === true ? (
+				<p>City not found</p>
+			) : (
+				''
+			)}
+		</>
+	);
 };
 export default InputCity;
--- weather-app/src/Components/WeatherDisplay.tsx
+++ weather-app/src/Components/WeatherDisplay.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import './App.css';
 type WeatherProps = {
-  name: string;
-  description: string;
-  temp: number;
-  humidity: number;
+	icon: string;
+	name: string;
+	description: string;
+	temp: number;
+	humidity: number;
 };
 const WeatherDisplay: React.FC<WeatherProps> = ({
-  name,
-  description,
-  temp,
-  humidity,
+	icon,
+	name,
+	description,
+	temp,
+	humidity,
 }) => {
-  return (
-    <div className='container'>
-      <div className='weather'>
-        <h2>Weather in {name}</h2>
-        <p>Description: {description}</p>
-        <p>Temperature: {temp}C</p>
-        <p>Humidity: {humidity}%</p>
-      </div>
-    </div>
-  );
+	return (
+		<div className='container'>
+			<div className='weather'>
+				<img src={icon} alt='weather icon' />
+				<h2>Weather in {name}</h2>
+				<p>Description: {description}</p>
+				<p>Temperature: {temp}C</p>
+				<p>Humidity: {humidity}%</p>
+			</div>
+		</div>
+	);
 };
 export default WeatherDisplay;
